02813b1c3d0ee93feda73394a69c558ab8670bb8
Casey Halstead <catherineclosee@gmail.com>
2022-02-01 17:54:41 -0600

    Add test for should be a function

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,2 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+describe('Turns', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function')
+  });
+});
