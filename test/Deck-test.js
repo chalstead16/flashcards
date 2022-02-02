bcc7fcfa55a409cb60523d6c0faddf91aaf7d717
Casey Halstead <catherineclosee@gmail.com>
2022-02-02 17:43:25 -0600

    Add test for deck property of cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,6 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
+//const Turn = require('../src/Turn');
+
 describe('Deck', function() {
 
   it('should be a function', function() {
@@ -13,5 +16,15 @@ describe('Deck', function() {
     const deck = new Deck();
 
     expect(deck).to.be.an.instanceof(Deck);
-  })
+  });
+
+  it('should have a property of cards', function(){
+    const cardOne = new Card(1,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
+    const cardTwo = new Card(2, 'What is Casey\'s favorite season?', ['winter', 'summer', 'autumn'], 'autumn');
+    const cardThree = new Card(3, 'What is Casey\'s favorite food?', ['the limit does not exist', 'meatballs', 'sushi'], 'the limit does not exist');
+
+    const deck = new Deck([cardOne, cardTwo, cardThree]);
+
+    expect(deck.cards).to.deep.equal([cardOne, cardTwo, cardThree]);
+  });
 });
