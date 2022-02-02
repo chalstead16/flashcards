14c9e1671321d8f0d0b49c5f20fea097e6f135c4
Casey Halstead <catherineclosee@gmail.com>
2022-02-02 17:25:09 -0600

    Add test for deck function

---
+++ test/Deck-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+
+    expect(Turn).to.be.a('function')
+  });
+});
