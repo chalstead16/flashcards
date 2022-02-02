92976a444b4c19209e0478952943eaf588cff64a
Casey Halstead <catherineclosee@gmail.com>
2022-02-01 18:27:47 -0600

    Add test for instance of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,4 +7,12 @@ describe('Turns', function() {
     const turn = new Turn();
     expect(Turn).to.be.a('function')
   });
+
+  it('should be an instance of Turn', fuction() {
+    const turn = new Turn();
+
+    expect(Turn).to.be.an.instanceof(Turn);
+  });
+
+   //with an argument to represent a user\'s guess to the question
 });
