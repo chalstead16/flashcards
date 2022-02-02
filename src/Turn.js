ca2a616844ad15131de0a8d034e9fbb7915a0145
Casey Halstead <catherineclosee@gmail.com>
2022-02-02 08:10:13 -0600

    Add Turn class and return guess method

---
+++ src/Turn.js
@@ -0,0 +1,12 @@
+class Turn {
+  constructor(guess, currentCard) {
+    this.guess = guess;
+    this.currentCard = currentCard;
+  }
+
+  returnGuess(){
+    return this.guess;
+  }
+}
+
+module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,29 +2,43 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 describe('Turns', function() {
 
   it('should be a function', function() {
     const turn = new Turn();
+
     expect(Turn).to.be.a('function')
   });
 
   it('should be an instance of Turn', function() {
     const turn = new Turn();
+
     expect(turn).to.be.an.instanceof(Turn)
   });
 
   it('should be able to store the user\'s guess for the question', function() {
     const card = new Card(2,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
     const turn = new Turn('blue');
+
     expect(turn.guess).to.deep.equal('blue')
   });
 
   it('should be able to store the user\'s guess for the question', function() {
     const card = new Card(2,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
     const turn = new Turn('blue', card);
+
     expect(turn.guess).to.deep.equal('blue')
     expect(turn.currentCard).to.deep.equal(card)
+  });
+
+  it('should be able to returns the user\'s guess for the question', function() {
+    const card = new Card(2,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
+    const turn = new Turn('blue', card);
+
+    expect(turn.guess).to.deep.equal('blue')
+    expect(turn.currentCard).to.deep.equal(card)
+    expect(turn.returnGuess(), 'blue')
   });
 });
