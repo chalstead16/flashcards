f6de77b52aee7b3bfc8cf7db5e9ecc3f16f53844
Casey Halstead <catherineclosee@gmail.com>
2022-02-02 19:28:56 -0600

    Add test for method to return current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -23,11 +23,16 @@ describe('Round', function() {
     const deck = new Deck([cardOne, cardTwo, cardThree]);
     const round = new Round(deck);
 
-
     expect(round.deck).to.equal([cardOne, cardTwo, cardThree]);
   });
-  //it should take in responses and records these guesses (turn)
 
-  //the `currentCard` should be the first `Card` in the `Deck` at the start of  `Round`
-  // property of currentCard should be the first card this.cards[Deck]
+  it('should have a way to return the current card', function() {
+    const cardOne = new Card(1,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
+    const cardTwo = new Card(2, 'What is Casey\'s favorite season?', ['winter', 'summer', 'autumn'], 'autumn');
+    const cardThree = new Card(3, 'What is Casey\'s favorite food?', ['the limit does not exist', 'meatballs', 'sushi'], 'the limit does not exist');
+    const deck = new Deck([cardOne, cardTwo, cardThree]);
+    const round = new Round(deck);
+
+    expect(round.returnCurrentCard()).to.equal(round.deck[0]);
+  });
 });
