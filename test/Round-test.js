bef7385ce0f6755d773452ffb690d164e50196bd
Casey Halstead <catherineclosee@gmail.com>
2022-02-02 19:33:39 -0600

    Add test for round property of turns

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,10 +1,11 @@
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe('Round', function() {
 
-  it('should be a function', function () {
+  it('should be a function', function() {
     const round = new Round();
 
     expect(Round).to.be.a('function');
@@ -16,7 +17,7 @@ describe('Round', function() {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('should have a property of deck', function () {
+  it('should have a property of deck', function() {
     const cardOne = new Card(1,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
     const cardTwo = new Card(2, 'What is Casey\'s favorite season?', ['winter', 'summer', 'autumn'], 'autumn');
     const cardThree = new Card(3, 'What is Casey\'s favorite food?', ['the limit does not exist', 'meatballs', 'sushi'], 'the limit does not exist');
@@ -24,6 +25,16 @@ describe('Round', function() {
     const round = new Round(deck);
 
     expect(round.deck).to.equal([cardOne, cardTwo, cardThree]);
+  });
+
+  it('should have a property to track the turns count', function() {
+    const cardOne = new Card(1,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
+    const cardTwo = new Card(2, 'What is Casey\'s favorite season?', ['winter', 'summer', 'autumn'], 'autumn');
+    const cardThree = new Card(3, 'What is Casey\'s favorite food?', ['the limit does not exist', 'meatballs', 'sushi'], 'the limit does not exist');
+    const deck = new Deck([cardOne, cardTwo, cardThree]);
+    const round = new Round(deck);
+
+    expect(round.turns).to.equal(1);
   });
 
   it('should have a way to return the current card', function() {
@@ -35,4 +46,12 @@ describe('Round', function() {
 
     expect(round.returnCurrentCard()).to.equal(round.deck[0]);
   });
+
+  // it('should have a way to update turn', function() {
+  //   const cardOne = new Card(1,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
+  //   const cardTwo = new Card(2, 'What is Casey\'s favorite season?', ['winter', 'summer', 'autumn'], 'autumn');
+  //   const cardThree = new Card(3, 'What is Casey\'s favorite food?', ['the limit does not exist', 'meatballs', 'sushi'], 'the limit does not exist');
+  //   const deck = new Deck([cardOne, cardTwo, cardThree]);
+  //   const round = new Round(deck);
+  // });
 });
