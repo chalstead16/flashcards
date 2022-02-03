0829f89d027a0e8e9bf6618a3fb9bcacd22366cd
Casey Halstead <catherineclosee@gmail.com>
2022-02-02 18:59:47 -0600

    Add test for round property of deck

--- test/Round-test.js
+++ test/Round-test.js
@@ -10,9 +10,24 @@ describe('Round', function() {
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
+  it('should be an instance of Round', function() {
     const round = new Round();
 
     expect(round).to.be.an.instanceof(Round);
   });
+
+  it('should have a property of deck', function () {
+    const cardOne = new Card(1,'What is Casey\'s favorite color?', ['blue', 'pink', 'orange'], 'orange');
+    const cardTwo = new Card(2, 'What is Casey\'s favorite season?', ['winter', 'summer', 'autumn'], 'autumn');
+    const cardThree = new Card(3, 'What is Casey\'s favorite food?', ['the limit does not exist', 'meatballs', 'sushi'], 'the limit does not exist');
+    const deck = new Deck([cardOne, cardTwo, cardThree]);
+    const round = new Round(deck);
+
+
+    expect(round.deck).to.equal([cardOne, cardTwo, cardThree]);
+  });
+  //it should take in responses and records these guesses (turn)
+
+  //the `currentCard` should be the first `Card` in the `Deck` at the start of  `Round`
+  // property of currentCard should be the first card this.cards[Deck]
 });
